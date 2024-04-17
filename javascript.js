f7a34f656046f214d465bbf96354b7b059384ded
swirly-way <s4m14z1l@gmail.com>
2024-04-17 19:49:17 +0100

    add if statement for all possible choices that give player choice victory and return a string with message

--- javascript.js
+++ javascript.js
@@ -11,5 +11,9 @@ function getComputerChoice() {
 // playerSelection and computerSelection and then return a string declaring the winner
 
 function playRound(playerSelection, computerSelection) {
-    
+    if (playerSelection === 'Rock' && computerSelection === 'Scissors' ||
+    playerSelection === 'Scissors' && computerSelection === 'Paper' ||
+    playerSelection === 'Paper' && computerSelection === 'Rock') {
+        return `You win! ${playerSelection} beats ${computerSelection}`
+    }
 }
