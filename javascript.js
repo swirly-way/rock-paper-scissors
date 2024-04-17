009b9d3436476b7657d623ec2ac85bd8eea90644
swirly-way <s4m14z1l@gmail.com>
2024-04-17 19:42:18 +0100

    add function to play a round taking playerSelection and computerSelection as parameters

--- javascript.js
+++ javascript.js
@@ -6,3 +6,10 @@ function getComputerChoice() {
     const choices = ['Rock', 'Paper', 'Scissors'];
     const randomChoice = choices[Math.floor(Math.random() * choices.length)];
 }
+
+// write a function that plays a single round of rock paper scissors taking two parameters:
+// playerSelection and computerSelection and then return a string declaring the winner
+
+function playRound(playerSelection, computerSelection) {
+    
+}
