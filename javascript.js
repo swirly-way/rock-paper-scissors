d85d20e3e7d77a2de614047a6e629d8d3ba3033a
swirly-way <s4m14z1l@gmail.com>
2024-04-18 19:19:48 +0100

    nest current functions within scope of new function that plays a game

--- javascript.js
+++ javascript.js
@@ -2,31 +2,44 @@
 
 
 
-function getComputerChoice() {
-    const choices = ['Rock', 'Paper', 'Scissors'];
-    const randomChoice = choices[Math.floor(Math.random() * choices.length)];
-    return randomChoice;
-}
+
 
 // write a function that plays a single round of rock paper scissors taking two parameters:
 // playerSelection and computerSelection and then return a string declaring the winner
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === 'Rock' && computerSelection === 'Scissors' ||
-    playerSelection === 'Scissors' && computerSelection === 'Paper' ||
-    playerSelection === 'Paper' && computerSelection === 'Rock') {
-        return `You win! ${playerSelection} beats ${computerSelection}`;
+
+
+
+function playGame(){
+    
+    function getComputerChoice() {
+        const choices = ['Rock', 'Paper', 'Scissors'];
+        const randomChoice = choices[Math.floor(Math.random() * choices.length)];
+        return randomChoice;
     }
-    else if (computerSelection === 'Rock' && playerSelection === 'Scissors' ||
-    computerSelection === 'Scissors' && playerSelection === 'Paper' ||
-    computerSelection === 'Paper' && playerSelection === 'Rock'
-    ) {
-        return `You lose! ${computerSelection} beats ${playerSelection}`;
+
+    const computerSelection = getComputerChoice();
+    const playerSelection = prompt();
+
+    function playRound(playerSelection, computerSelection) {
+        if (playerSelection === 'Rock' && computerSelection === 'Scissors' ||
+        playerSelection === 'Scissors' && computerSelection === 'Paper' ||
+        playerSelection === 'Paper' && computerSelection === 'Rock') {
+            return `You win! ${playerSelection} beats ${computerSelection}`;
+        }
+        else if (computerSelection === 'Rock' && playerSelection === 'Scissors' ||
+        computerSelection === 'Scissors' && playerSelection === 'Paper' ||
+        computerSelection === 'Paper' && playerSelection === 'Rock'
+        ) {
+            return `You lose! ${computerSelection} beats ${playerSelection}`;
+        }
+        else {
+            return `Draw! ${computerSelection} and ${playerSelection} are matched!`
+        }
     }
-    else {
-        return `Draw! ${computerSelection} and ${playerSelection} are matched!`
-    }
+
+
 }
 
-const computerSelection = getComputerChoice();
-const playerSelection = prompt();
+
+
