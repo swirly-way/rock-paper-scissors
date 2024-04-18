fdc4b8d2bb57b4b0972fbc82e9ba7b0884c1e921
swirly-way <s4m14z1l@gmail.com>
2024-04-18 19:47:38 +0100

    move score variables inside game function scope

--- javascript.js
+++ javascript.js
@@ -1,9 +1,7 @@
 
-let playerScore = 0;
-let computerScore = 0;
+
 
 function playGame(){
-    
     function getComputerChoice() {
         const choices = ['Rock', 'Paper', 'Scissors'];
         const randomChoice = choices[Math.floor(Math.random() * choices.length)];
@@ -12,17 +10,22 @@ function playGame(){
 
     const computerSelection = getComputerChoice();
     const playerSelection = prompt();
+    
+    let playerScore = 0;
+    let computerScore = 0; 
 
     function playRound(playerSelection, computerSelection) {
         if (playerSelection === 'Rock' && computerSelection === 'Scissors' ||
         playerSelection === 'Scissors' && computerSelection === 'Paper' ||
         playerSelection === 'Paper' && computerSelection === 'Rock') {
             return `You win! ${playerSelection} beats ${computerSelection}`;
+            
         }
         else if (computerSelection === 'Rock' && playerSelection === 'Scissors' ||
         computerSelection === 'Scissors' && playerSelection === 'Paper' ||
         computerSelection === 'Paper' && playerSelection === 'Rock'
         ) {
+            
             return `You lose! ${computerSelection} beats ${playerSelection}`;
         }
         else {
@@ -34,7 +37,10 @@ function playGame(){
 }
 
 for (let i = 0; i <= 4; i++){
-    playGame();
+ playGame();
 }
+
+
+
 
 
